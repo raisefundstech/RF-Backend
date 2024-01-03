16db1a6f86d434fa1445d5a8a88ca09d09280bb9
030821958SA <Akhil.Deshneni-SA@csulb.edu>
2024-01-03 12:40:35 -0800

    updated destination location

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 # If the directory, `dist`, doesn't exist, create `dist`
-stat build || mkdir build
+stat dist || mkdir dist
 # Archive artifacts
 # cd build
-zip build/Finder-backend.zip -r build package.json config .platform node_modules .platform
+zip dist/Finder-backend.zip -r dist package.json config .platform node_modules .platform
